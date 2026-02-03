da94475174cf0f786281b4fd83fe193c51ee0c6a
assylhd <smailaseke@gmail.com>
2026-02-03 22:47:34 +0500

    if statement in Python

---
+++ Prac2/if-else/if.py
@@ -0,0 +1,31 @@
+#Python Conditions and If statements
+"""Python supports the usual logical conditions from mathematics:
+
+Equals: a == b
+Not Equals: a != b
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b
+These conditions can be used in several ways, most commonly in "if statements" and loops."""
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+
+number = 15
+if number > 0:
+  print("The number is positive")
+
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+
